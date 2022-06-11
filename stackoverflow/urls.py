abe519a47ebdba43a1f559b6f284482faf45f4d7
Aftab Ansari <aftab.ans9818@gmail.com>
2022-06-11 15:16:34 +0530

    static files

--- stackoverflow/urls.py
+++ stackoverflow/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from django.urls import include, path
-
+from django.urls import include, path, re_path
+from django.views.static import serve
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('', include('main.urls')),
+    re_path(r'media/(?P<path>.*)$', serve,
+            {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'static/(?P<path>.*)$', serve,
+            {'document_root': settings.STATIC_ROOT}),
 ]
