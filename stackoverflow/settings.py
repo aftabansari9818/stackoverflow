158c1d9adf96465853e26a3c1df48e1cd2d4b195
Aftab Ansari <aftab.ans9818@gmail.com>
2022-06-11 13:39:38 +0530

    init

--- stackoverflow/settings.py
+++ stackoverflow/settings.py
@@ -10,6 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-$8!w25-3ir=yzzngew+smwsk3+@p_lhulum%+#@(5b!!=7tf$g'
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
